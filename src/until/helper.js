bd64b3844125fe175fe514837fa093e437e6d2e3
unknown <=nishantjambhulkar6@gmail.com>
2022-06-02 07:39:08 +0530

    Assigment/nodejs-module

---
+++ src/until/helper.js
@@ -0,0 +1,8 @@
+const piyush = function(){
+    const Date1 = new Date();
+    console.log(Date1.getDate())
+    const Month1 = new Date();
+    console.log(Month1.getMonth()); // (January gives 0)
+    console.log('Print Batch information Name:Manish Sanjay Patil. Week 2 second day is 01/06 and topic being taught today is cover topic=> "Radon , W3D1,the topic for today is Nodejs module system"');
+    }
+    module.exports.nishu = piyush
---
+++ src/validator/formatter.js
@@ -0,0 +1,8 @@
+const f = function(){
+    let firstName = ' Manish '
+    console.log(firstName)
+    console.log(firstName.trim())
+    console.log(firstName.toLowerCase())
+    console.log(firstName.toUpperCase())
+}
+module.exports.form = f
