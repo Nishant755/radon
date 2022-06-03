a3bce4e7bafe0b0832c99090e554b435ad39be9b
unknown <=nishantjambhulkar6@gmail.com>
2022-06-03 08:29:28 +0530

    assigment/solution4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const myHelper = require('../util/helper')
 const underscore = require('underscore')
+const lodash=require("lodash")
 
 const router = express.Router();
 
@@ -8,16 +9,27 @@ router.get('/test-me', function (req, res) {
     myHelper.printDate()
     myHelper.getCurrentMonth()
     myHelper.getCohortData()
+    
     let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
     console.log('The first element received from underscope function is '+firstElement)
     res.send('My first ever api!')
 });
-
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-
+    router.get('/day12', function (req, res) {
+        const array =["jan","feb","march","april","may","june","july","august","septemb","oct","nov","dec"]
+        console.log(lodash.chunk(array,4))
+        console.log(".................1..................")
+        const array1 = [1,3,5,7,9,11,13,15,17,19]
+        console.log(lodash.tail(array1))
+        console.log(".................2..................")
+        const a =[50,51]
+        const b =[52,53]
+        const c =[54,55]
+        const d =[56,57]
+        const e =[58,59]
+        console.log(lodash.union(a,b,c,d,e))
+        console.log(".................3..................")
+         res.send('Hello User!!')
+     });
 router.get('/candidates', function(req, res){
     console.log('Query paramters for this request are '+JSON.stringify(req.query))
     let gender = req.query.gender
